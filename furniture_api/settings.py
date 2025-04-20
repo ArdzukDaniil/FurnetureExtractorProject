0135fb9658d78f8e9aef1b96ffa28109214fd561
Daniil Ardzuk <ardzyukdaniil@gmail.com>
2025-04-21 00:12:45 +0300

    Update ALLOWED_HOSTS for Render deployment

--- furniture_api/settings.py
+++ furniture_api/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', "django-insecure-krw!a%c5(alh(^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['furniture-product-extractor.onrender.com', '127.0.0.1', 'localhost']
 
 # Application definition
 
@@ -117,7 +117,7 @@ USE_TZ = True
 STATIC_URL = "static/"
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
